38f567d96ad9a37eb635fd8d6e3747902163fcde
Juan Miguel <juanmiblancof@gmail.com>
2021-04-02 12:13:17 +0200

    @juablafer - Added prefix option to allow multiple environments

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+backup='backup'
 timestamp_start=`date +%Y-%m-%d_%H-%M-%S`
 echo "Cron job started at $timestamp_start"
 
@@ -8,7 +9,7 @@ mongodump --uri mongodb://$DB_CONTAINER:$DB_PORT/$DB_NAME -o /backup
 echo "Backup generated successfully"
 
 echo "Uploading backup to S3"
-aws s3 cp /backup/$DB_NAME/ s3://$S3_BUCKET_NAME/backup-$timestamp_start/ --recursive
+aws s3 cp /backup/$DB_NAME/ s3://$S3_BUCKET_NAME/$PREFIX$backup-$timestamp_start/ --recursive
 echo "Backup uploaded successfully"
 
 timestamp_end=`date +%Y-%m-%d_%H-%M-%S`
